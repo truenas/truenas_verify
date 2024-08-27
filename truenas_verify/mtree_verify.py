99cff16a11250935ce16e40a2f17c76a914103e2
Logan Cary <logan.cary@ixsystems.com>
2024-08-27 16:30:39 -0400

    save memory with sets

--- truenas_verify/mtree_verify.py
+++ truenas_verify/mtree_verify.py
@@ -62,52 +62,52 @@ def validate_file_sha256sum(entry, errors):
     with open(entry.fname, 'rb', buffering=0) as f:
         hash = file_digest(f, 'sha256').hexdigest()
         if hash != entry.sha256:
-            errors.append(f'{entry.fname}: expected: {entry.sha256}, got: {hash}')
+            errors.add(f'{entry.fname}: expected: {entry.sha256}, got: {hash}')
 
 
-def validate_mtree_entry(entry) -> list[str]:
+def validate_mtree_entry(entry) -> set[str]:
     try:
         st = lstat(entry.fname)
     except FileNotFoundError:
-        return [f'{entry.fname}: file does not exist.']
+        return {f'{entry.fname}: file does not exist.'}
 
-    errors = []
+    errors = set()
     if st.st_uid != entry.uid:
-        errors.append(f'{entry.fname}: got uid {st.st_uid}, expected: {entry.uid}')
+        errors.add(f'{entry.fname}: got uid {st.st_uid}, expected: {entry.uid}')
     if st.st_gid != entry.gid:
-        errors.append(f'{entry.fname}: got gid {st.st_gid}, expected: {entry.gid}')
+        errors.add(f'{entry.fname}: got gid {st.st_gid}, expected: {entry.gid}')
 
     match entry.type:
         case 'dir':
             if not S_ISDIR(st.st_mode):
-                errors.append(f'{entry.fname}: incorrect file type.')
+                errors.add(f'{entry.fname}: incorrect file type.')
         case 'file':
             if not S_ISREG(st.st_mode):
-                errors.append(f'{entry.fname}: incorrect file type.')
+                errors.add(f'{entry.fname}: incorrect file type.')
 
             validate_file_sha256sum(entry, errors)
         case 'link':
             if not S_ISLNK(st.st_mode):
-                errors.append(f'{entry.fname}: incorrect file type.')
+                errors.add(f'{entry.fname}: incorrect file type.')
 
     if oct(S_IMODE(st.st_mode))[2:] != entry.mode:
-        errors.append(f'{entry.fname}: got mode {oct(S_IMODE(st.st_mode))}, expected: {entry.mode}')
+        errors.add(f'{entry.fname}: got mode {oct(S_IMODE(st.st_mode))}, expected: {entry.mode}')
 
     return errors
 
 
-def process_chunk(chunk) -> list[str]:
-    errors = []
+def process_chunk(chunk) -> set[str]:
+    errors = set()
     for line in chunk:
         if (entry := parse_mtree_entry(line)) is not None:
-            errors.extend(validate_mtree_entry(entry))
+            errors.update(validate_mtree_entry(entry))
     return errors
 
 
 def main():
     with Pool(min(cpu_count(), 6)) as pool, open(MTREE_FILE_PATH, 'r') as f:
         results = pool.imap_unordered(process_chunk, batched(f, CHUNK_SIZE))
-        errors = [e for r in results for e in r]
+        errors = {e for r in results for e in r}
 
     if errors:
         with open(LOG_PATH, 'w') as f:
